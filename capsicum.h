190aac7c694e985943ae6199e5f9e4343ae8c4dd
David Drysdale <drysdale@google.com>
2014-02-14 17:15:43 +0000

    Move rights manipulation code into separate header (capsicum-rights.h)

--- capsicum.h
+++ capsicum.h
@@ -3,6 +3,7 @@
  */
 #ifndef __CAPSICUM_H__
 #define __CAPSICUM_H__
+#include "capsicum-rights.h"
 
 /************************************************************
  * FreeBSD
@@ -184,102 +185,9 @@ inline int cap_rights_limit(int fd, const cap_rights_t *rights) {
 }
 #endif
 
-#ifndef CAP_RIGHTS_VERSION
-/************************************************************
- * Capsicum compatibility layer: implement new (FreeBSD10.x)
- * rights manipulation API in terms of original (FreeBSD9.x)
- * functionality.
- ************************************************************/
-#include <stdarg.h>
 #include <stdio.h>
-#include <unistd.h>
-
-/* Rights manipulation macros/functions */
-#define cap_rights_init(rights, ...)   _cap_rights_init((rights), __VA_ARGS__, 0ULL)
-#define cap_rights_set(rights, ...)    _cap_rights_set((rights), __VA_ARGS__, 0ULL)
-#define cap_rights_clear(rights, ...)  _cap_rights_clear((rights), __VA_ARGS__, 0ULL)
-#define cap_rights_is_set(rights, ...) _cap_rights_is_set((rights), __VA_ARGS__, 0ULL)
-
-inline cap_rights_t* _cap_rights_init(cap_rights_t *rights, ...) {
-  va_list ap;
-  cap_rights_t right;
-  *rights = 0;
-  va_start(ap, rights);
-  while (true) {
-    right = va_arg(ap, cap_rights_t);
-    *rights |= right;
-    if (right == 0) break;
-  }
-  va_end(ap);
-  return rights;
-}
-
-inline cap_rights_t* _cap_rights_set(cap_rights_t *rights, ...) {
-  va_list ap;
-  cap_rights_t right;
-  va_start(ap, rights);
-  while (true) {
-    right = va_arg(ap, cap_rights_t);
-    *rights |= right;
-    if (right == 0) break;
-  }
-  va_end(ap);
-  return rights;
-}
-
-inline cap_rights_t* _cap_rights_clear(cap_rights_t *rights, ...) {
-  va_list ap;
-  cap_rights_t right;
-  va_start(ap, rights);
-  while (true) {
-    right = va_arg(ap, cap_rights_t);
-    *rights &= ~right;
-    if (right == 0) break;
-  }
-  va_end(ap);
-  return rights;
-}
-
-inline bool _cap_rights_is_set(const cap_rights_t *rights, ...) {
-  va_list ap;
-  cap_rights_t right;
-  cap_rights_t accumulated = 0;
-  va_start(ap, rights);
-  while (true) {
-    right = va_arg(ap, cap_rights_t);
-    accumulated |= right;
-    if (right == 0) break;
-  }
-  va_end(ap);
-  return (accumulated & *rights) == accumulated;
-}
-
-inline bool _cap_rights_is_valid(const cap_rights_t *rights) {
-  return true;
-}
-
-inline cap_rights_t* cap_rights_merge(cap_rights_t *dst, const cap_rights_t *src) {
-  *dst |= *src;
-  return dst;
-}
-
-inline cap_rights_t* cap_rights_remove(cap_rights_t *dst, const cap_rights_t *src) {
-  *dst &= ~(*src);
-  return dst;
-}
-
-inline bool cap_rights_contains(const cap_rights_t *big, const cap_rights_t *little) {
-  return ((*big) & (*little)) == (*little);
-}
-
-inline void cap_rights_describe(const cap_rights_t *rights, char *buffer) {
-  sprintf(buffer, "0x%016llx", (*rights));
-}
-
-#else
-
-/* New-style Capsicum API extras */
-#include <stdio.h>
+#ifdef CAP_RIGHTS_VERSION
+/* New-style Capsicum API extras for debugging */
 inline void cap_rights_describe(const cap_rights_t *rights, char *buffer) {
   for (int ii = 0; ii < (CAP_RIGHTS_VERSION+2); ii++) {
     int len = sprintf(buffer, "0x%016llx ", (unsigned long long)rights->cr_rights[ii]);
@@ -296,6 +204,12 @@ inline std::ostream& operator<<(std::ostream& os, cap_rights_t rights) {
   return os;
 }
 
-#endif  /* old/new style rights manipulation */
+#else
+
+inline void cap_rights_describe(const cap_rights_t *rights, char *buffer) {
+  sprintf(buffer, "0x%016llx", (*rights));
+}
+
+#endif  /* new/old style rights manipulation */
 
 #endif /*__CAPSICUM_H__*/
