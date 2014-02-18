78b460210d48281255f48adade950ce89c931cc0
David Drysdale <drysdale@google.com>
2014-02-18 16:27:50 +0000

    Move OS-specific chunks into separate headers

---
+++ capsicum-freebsd.h
@@ -0,0 +1,36 @@
+#ifndef __CAPSICUM_FREEBSD_H__
+#define __CAPSICUM_FREEBSD_H__
+#ifdef __FreeBSD__
+/************************************************************
+ * FreeBSD Capsicum Functionality.
+ ************************************************************/
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* FreeBSD definitions. */
+#include <sys/param.h>
+#include <sys/capability.h>
+#include <sys/procdesc.h>
+
+#if __FreeBSD_version >= 1000000
+#define AT_SYSCALLS_IN_CAPMODE
+#define HAVE_CAP_RIGHTS_GET
+#define HAVE_CAP_RIGHTS_LIMIT
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+// Use fexecve_() in tests to allow Linux variant to bypass glibc version.
+#define fexecve_(F, A, E) fexecve(F, A, E)
+
+// TODO(FreeBSD): uncomment if/when FreeBSD propagates rights on accept.
+// FreeBSD does not generate a capability from accept(cap_fd,...)
+// #define CAP_FROM_ACCEPT
+
+#endif  /* __FreeBSD__ */
+
+#endif /*__CAPSICUM_FREEBSD_H__*/
--- capsicum.h
+++ capsicum.h
@@ -3,117 +3,21 @@
  */
 #ifndef __CAPSICUM_H__
 #define __CAPSICUM_H__
-#include "capsicum-rights.h"
 
-/************************************************************
- * FreeBSD
- ************************************************************/
 #ifdef __FreeBSD__
-
-#ifdef __cplusplus
-extern "C" {
+#include "capsicum-freebsd.h"
 #endif
 
-/* FreeBSD definitions */
-#include <sys/param.h>
-#include <sys/capability.h>
-#include <sys/procdesc.h>
-#if __FreeBSD_version >= 1000000
-#define AT_SYSCALLS_IN_CAPMODE
-#define HAVE_CAP_RIGHTS_GET
-#define HAVE_CAP_RIGHTS_LIMIT
-#endif
-
-#ifdef __cplusplus
-}
-#endif
-
-// Use fexecve_() in tests to allow Linux variant to bypass glibc version.
-#define fexecve_(F, A, E) fexecve(F, A, E)
-
-// TODO(FreeBSD): uncomment if/when FreeBSD propagates rights on accept.
-// FreeBSD does not generate a capability from accept(cap_fd,...)
-// #define CAP_FROM_ACCEPT
-
-#endif  /* FreeBSD */
-
-/************************************************************
- * Linux
- ************************************************************/
 #ifdef __linux__
-#include <errno.h>
-#include <unistd.h>
-#include <sys/prctl.h>
-#include <sys/resource.h>
-#include <sys/syscall.h>
-#include <linux/seccomp.h>
-#include <linux/capsicum.h>
-#include <linux/procdesc.h>
-
-#define HAVE_PDWAIT4
-#define CAP_FROM_ACCEPT
-#define AT_SYSCALLS_IN_CAPMODE
-
-#ifdef __cplusplus
-extern "C" {
+#include "capsicum-linux.h"
 #endif
-
-inline int cap_enter() {
-  int rc = prctl(PR_SET_NO_NEW_PRIVS, 1, 0, 0, 0);
-  if (rc < 0) return rc;
-  return prctl(PR_SET_SECCOMP, SECCOMP_MODE_LSM);
-}
-
-inline int cap_getmode(unsigned int *mode) {
-  int rc = prctl(PR_GET_SECCOMP);
-  if (rc < 0) return rc;
-  *mode = (rc == SECCOMP_MODE_LSM);
-  return 0;
-}
-
-#define HAVE_CAP_RIGHTS_LIMIT
-inline int cap_rights_limit(int fd, cap_rights_t *rights) {
-  return syscall(__NR_cap_rights_limit, fd, rights);
-}
-
-#define HAVE_CAP_RIGHTS_GET
-inline int cap_rights_get(int fd, cap_rights_t *rights) {
-  return syscall(__NR_cap_rights_get, fd, rights);
-}
-
-// Linux glibc includes an fexecve() function, implemented via the /proc
-// filesystem.  Bypass this and go directly to the fexecve syscall.
-inline int fexecve_(int fd, char **argv, char **envp)
-{
-  return syscall(__NR_fexecve, fd, argv, envp);
-}
-
-inline int pdfork(int *fd, int flags) {
-  return syscall(__NR_pdfork, fd, flags);
-}
-
-inline int pdgetpid(int fd, pid_t *pid) {
-  return syscall(__NR_pdgetpid, fd, pid);
-}
-
-inline int pdkill(int fd, int signum) {
-  return syscall(__NR_pdkill, fd, signum);
-}
-
-inline int pdwait4(int fd, int *status, int options, struct rusage *rusage) {
-  return syscall(__NR_pdwait4, fd, status, options, rusage);
-}
-
-#ifdef __cplusplus
-}
-#endif
-
-#endif  /* Linux */
 
 /************************************************************
  * Define new-style rights in terms of old-style rights if
  * absent.
  ************************************************************/
+#include "capsicum-rights.h"
+
 #ifdef CAP_PREAD
 /* Existence of CAP_PREAD implies new-style CAP_SEEK semantics */
 #define CAP_SEEK_ASWAS 0
