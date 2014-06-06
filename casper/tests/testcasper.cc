ddfa097e0a4c15e7fb11ad7c12aae737f1a2b957
David Drysdale <drysdale@google.com>
2014-06-06 11:20:38 +0100

    Use C++11 for casper tests

--- casper/tests/testcasper.cc
+++ casper/tests/testcasper.cc
@@ -6,6 +6,15 @@ extern const char *casper_sock;
 
 TEST(Casper, Init) {
   cap_channel_t *chan = cap_init_sock(casper_sock);
-  //@@ EXPECT_NE((cap_channel_t *)NULL, chan);
-  if (chan) cap_close(chan);
+  if (!chan) {
+    fprintf(stderr, "Skipping test as cap_init_sock('%s') failed\n", casper_sock);
+    return;
+  }
+  EXPECT_NE(nullptr, chan);
+  cap_close(chan);
+}
+
+TEST(Casper, InitFail) {
+  cap_channel_t *chan = cap_init_sock("bogus");
+  EXPECT_EQ(nullptr, chan);
 }
