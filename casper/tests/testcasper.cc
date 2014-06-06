e3e5b686e06278ad68ed84fcefc1565ac0549b87
David Drysdale <drysdale@google.com>
2014-06-06 13:20:50 +0100

    Add socket wrap/unwrap test

--- casper/tests/testcasper.cc
+++ casper/tests/testcasper.cc
@@ -1,3 +1,6 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+
 #include <libcapsicum.h>
 
 #include "gtest/gtest.h"
@@ -17,4 +20,26 @@ TEST(Casper, Init) {
 TEST(Casper, InitFail) {
   cap_channel_t *chan = cap_init_sock("bogus");
   EXPECT_EQ(nullptr, chan);
+}
+
+TEST(Casper, SocketWrap) {
+  cap_channel_t *chan = cap_init_sock(casper_sock);
+  if (!chan) {
+    fprintf(stderr, "Skipping test as cap_init_sock('%s') failed\n", casper_sock);
+    return;
+  }
+
+  int sock_fds[2];
+  EXPECT_EQ(0, socketpair(AF_UNIX, SOCK_STREAM, 0, sock_fds));
+
+  cap_channel_t *sock_chan = cap_wrap(sock_fds[0]);
+  EXPECT_NE(nullptr, sock_chan);
+  EXPECT_EQ(sock_fds[0], cap_sock(sock_chan));
+
+  int fd = cap_unwrap(sock_chan);  // Frees sock_chan
+  EXPECT_EQ(sock_fds[0], fd);
+
+  close(sock_fds[1]);
+  close(sock_fds[0]);
+  cap_close(chan);
 }
