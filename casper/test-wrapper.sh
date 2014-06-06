3caccfaafe61f0abc0121e4b8c40b2614d72d288
David Drysdale <drysdale@google.com>
2014-06-06 11:12:48 +0100

    Add wrapper script to run tests while local casperd is running

---
+++ casper/test-wrapper.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+SOCKFILE=./casperd.sock
+PIDFILE=./casperd.pid
+ETCDIR=./etc
+VERBOSE="-v -v -v"
+
+# Run the daemon but not daemonized (-F)
+./casperd -F -D $ETCDIR -P $PIDFILE -S $SOCKFILE $VERBOSE &
+
+# Run the unit tests while the daemon is running
+./casper-test -S $SOCKFILE
+
+# Terminate the daemon
+kill `cat $PIDFILE`
