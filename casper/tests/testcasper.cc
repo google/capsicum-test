a08eec779d8536db85f28fac209ce605cb08baeb
David Drysdale <drysdale@google.com>
2014-06-09 13:46:25 +0100

    Add cap_gethostbyname() test

--- casper/tests/testcasper.cc
+++ casper/tests/testcasper.cc
@@ -1,10 +1,14 @@
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <netdb.h>
 
 #include <libcapsicum.h>
+#include <libcapsicum_service.h>
+#include <libcapsicum_dns.h>
 
 #include "gtest/gtest.h"
 
+extern bool verbose;
 extern const char *casper_sock;
 
 TEST(Casper, Init) {
@@ -42,4 +46,32 @@ TEST(Casper, SocketWrap) {
   close(sock_fds[1]);
   close(sock_fds[0]);
   cap_close(chan);
+}
+
+TEST(Casper, GetHostByName) {
+  cap_channel_t *chan = cap_init_sock(casper_sock);
+  if (!chan) return;
+
+  cap_channel_t *dns_chan = cap_service_open(chan, "system.dns");
+  EXPECT_NE(nullptr, dns_chan) << "Failed to open system.dns service";
+  if (!dns_chan) return;
+  cap_close(chan);
+
+  const char *name = "google.com.";
+  struct hostent *info = cap_gethostbyname(dns_chan, name);
+  EXPECT_NE(nullptr, info);
+
+  if (verbose && info) {
+    fprintf(stderr, "'%s' -> '%s'", name, info->h_name);
+    int ii = 0;
+    while (info->h_addr_list && info->h_addr_list[ii]) {
+      for (int jj = 0; jj < info->h_length; jj++) {
+        fprintf(stderr, "%s%d", (jj>0) ? "." : ", ", (unsigned char)info->h_addr_list[ii][jj]);
+      }
+      ii++;
+    }
+    fprintf(stderr, "\n");
+  }
+
+  cap_close(dns_chan);
 }
