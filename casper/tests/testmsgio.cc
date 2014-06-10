695ffc7cbc0cce513aa1a3f625db6cf2e85ae29f
David Drysdale <drysdale@google.com>
2014-06-10 18:06:30 +0100

    Split out test that uses msgio.h
    The msgio.h header is internal to libnv, so separate out that test from
    the tests that hit the public interface.

---
+++ casper/tests/testmsgio.cc
@@ -0,0 +1,53 @@
+#include "nv.h"
+#include "msgio.h"
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#include <string>
+
+#include "gtest/gtest.h"
+
+extern bool verbose;
+static nvlist_t *nvnull = NULL;
+
+TEST(NVList, CredSend) {
+  int fds[2];
+  EXPECT_EQ(0, socketpair(AF_UNIX, SOCK_STREAM, 0, fds));
+
+  // Need to turn on SO_PASSCRED before the fork(); for some reason, turning
+  // it on afterwards doesn't work (and credentials are always returned as
+  // 65534/65534)
+  int one = 1;
+  EXPECT_EQ(0, setsockopt(fds[0], SOL_SOCKET, SO_PASSCRED, &one, sizeof(one)));
+
+  pid_t child = fork();
+  if (child == 0) {
+    // Child: wait to receive credentials.
+    uid_t uid;
+    gid_t gid;
+    gid_t groups[10];
+    int ngroups = 10;
+    EXPECT_EQ(0, cred_recv(fds[0], &uid, &gid, &ngroups, groups));
+    EXPECT_EQ(getuid(), uid);
+    EXPECT_EQ(getgid(), gid);
+    exit(HasFailure());
+  }
+
+  // Send credentials down the socket.
+  EXPECT_EQ(0, cred_send(fds[1]));
+
+  // Wait for the child.
+  int status;
+  EXPECT_EQ(child, waitpid(child, &status, 0));
+  EXPECT_TRUE(WIFEXITED(status)) << " status " << status;
+  EXPECT_EQ(0, WEXITSTATUS(status));
+
+  close(fds[1]);
+  close(fds[0]);
+}
+
